232d3498616a5859ce0233f5cb958941d0b9ece3
nate <nathanael.frappart@ryzhom.io>
2021-01-16 22:43:25 +0100

    fix variables - nate

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "azurerm_subnet" "TerraSubnet" {
   name                      = var.SubnetName
   resource_group_name       = var.RgName
   virtual_network_name      = var.VNetName
-  #address_prefix            = var.SubnetAddressPrefix
   address_prefixes            = var.SubnetAddressPrefix
   #below command is deprecated but necessary to avoid resource delete/add by terraform.
   #See https://github.com/terraform-providers/terraform-provider-azurerm/issues/2526 for reference
